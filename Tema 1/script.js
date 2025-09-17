b538a023b4528c80aa3b3f34d3bbe2c0a4d4f21a
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-09-17 12:41:13 +0200

    Resolvemos ejercicio 3 y 4

--- Tema 1/script.js
+++ Tema 1/script.js
@@ -1,3 +1,4 @@
+// Ejercicio 1
 function saludar(){
             var edad = window.prompt("Introduce tu edad");
             if (edad >= 18) {
@@ -7,6 +8,20 @@ function saludar(){
             }
         }
 
+//Ejercicio 2
 function pintar_ruta(){
-    console.log("El acceso a la ruta C:\\\\usuario\\ tarda 1'23"," algo considerado  en la actualidad.");
+    console.log("El acceso a la ruta C:\\\\usuario\\tarda 1'23\", algo considerado \"lento\" en la actualidad.");
+}
+
+//Ejercicio 3
+function pintar_ruta_v2(){
+    var subtrng1 = "El acceso a la ruta C:\\\\usuario\\tarda 1'23\", algo";
+    var subtrng2 = "considerado \"lento\" en la actualidad.";
+    console.log(subtrng1 + subtrng2);
+}
+
+//Ejercicio 4
+function mostrar_potencia(){
+    var num = 2*Math.pow(10,-9);
+    console.log("El valor de 2*10^-9 es: " + num);
 }
