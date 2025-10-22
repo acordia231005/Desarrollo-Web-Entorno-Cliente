75119bcec2a196a2a3a7c6ad0824422a7148f117
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-10-22 11:15:05 +0200

    hasta actividad 6

--- Tema_2=Typescript/Boletin1/Script.js
+++ Tema_2=Typescript/Boletin1/Script.js
@@ -1,3 +1,7 @@
+// Para iniciarlo . Dsp de ir a la carpeta de 
+// antes del Typescript ejecutar este comando
+// tsc .\Script.ts 
+// Boletin 1
 // Ejercicio 1
 function ej1() {
     var hora = new Date;
@@ -8,15 +12,51 @@ function ej1() {
 }
 // Ejercicio 2
 function ej2() {
-    var expReg = new RegExp("lo_q_sea");
-    var mensaje = document.createElement("span");
-    if (document.getElementById("email") != null) {
-        var email = document.getElementById("email");
-        if (expReg.test(email.value)) {
-            mensaje.textContent = "Email correcto";
-        }
-        else {
-            mensaje.textContent = "Email incorrecto";
-        }
+    var regExp = new RegExp("[^\s@]+@+[^\s@]+\.+[^\s@]+$"); // Con fallos ( hay que solucionar)
+    // Rescatamos las partes ( nodos ) necesarios.
+    var parrafoOk = document.getElementById("ok"); //Escritura
+    var parrafoError = document.getElementById("error"); //Escritura
+    //Aplico la validacion ( $inputValue me da el valor del "email") 
+    // y miro con el $writeNode si es valido o no ( rudimentario )
+    if (regExp.test($inputValue("email"))) {
+        $writeNode("ok", "Email valido");
+        $writeNode("error", "");
+    }
+    else {
+        $writeNode("ok", "");
+        $writeNode("error", "Email no valido");
+    }
+}
+// Ejercicio 3
+function ej3() {
+    var ventana = window.open("https:www.google.es", "nuevaventana");
+    ventana === null || ventana === void 0 ? void 0 : ventana.document.write("hola");
+}
+// Ejercicio 4
+function ej4() {
+    var regexp = new RegExp("^https:\/\/");
+    var url = $inputValue("url");
+    if (regexp.test(url)) {
+        window.location.href = url;
+    }
+    else {
+        $writeNode("error2", " una url valida");
+        setTimeout(function () { return $writeNode("error2", ""); }, 5000);
+    }
+}
+// Helpers ( comunes para todo el boletin.SIEMPRE EMPIEZAN POR DOLAR) 
+// (AQUI PILLAN EL VALOR DEL INPUT Y EL WRITE NODE ESCRIBE EL MENSAJE )
+function $inputValue(id) {
+    var input = document.getElementById(id); // Lectura
+    var result = "";
+    if (input) {
+        result = input.value; // Aqui leo el valor
+    }
+    return result;
+}
+function $writeNode(id, msg) {
+    var nodo = document.getElementById(id); //Escritura
+    if (nodo) {
+        nodo.textContent = msg;
     }
 }
--- Tema_2=Typescript/Boletin1/Script.ts
+++ Tema_2=Typescript/Boletin1/Script.ts
@@ -15,14 +15,65 @@ function ej1() {
 
 // Ejercicio 2
 function ej2() {
-    let expReg: RegExp = new RegExp("lo_q_sea");
-    let mensaje = document.createElement("span");
-    if (document.getElementById("email") != null) {
-        let email: HTMLInputElement = document.getElementById("email") as HTMLInputElement;        
-        if(expReg.test(email.value)){
-            mensaje.textContent = "Email correcto"
-        }else{
-            mensaje.textContent = "Email incorrecto"
-        }
+    const regExp = new RegExp("[^\s@]+@+[^\s@]+\.+[^\s@]+$"); // Con fallos ( hay que solucionar)
+
+    // Rescatamos las partes ( nodos ) necesarios.
+    const parrafoOk = document.getElementById("ok") as HTMLElement; //Escritura
+    const parrafoError = document.getElementById("error") as HTMLElement; //Escritura
+
+
+    //Aplico la validacion ( $inputValue me da el valor del "email") 
+    // y miro con el $writeNode si es valido o no ( rudimentario )
+
+    if(regExp.test($inputValue("email"))){
+       $writeNode("ok", "Email valido");
+       $writeNode("error", "");
+    }else{
+        $writeNode("ok", "");
+        $writeNode("error", "Email no valido");
+    }
+}
+
+// Ejercicio 3
+function ej3(){
+    let ventana = window.open("https:www.google.es", "nuevaventana");
+    ventana?.document.write("hola");
+}
+
+// Ejercicio 4
+function ej4() : void{
+    const regexp = new RegExp("^https:\/\/");
+    const url = $inputValue("url");
+    if (regexp.test(url)) {
+        window.location.href = url;
+    }else{
+        $writeNode("error2", " una url valida");
+        setTimeout(() => $writeNode("error2", ""), 5000);
+    }
+    
+}
+
+
+
+
+// Helpers ( comunes para todo el boletin.SIEMPRE EMPIEZAN POR DOLAR) 
+// (AQUI PILLAN EL VALOR DEL INPUT Y EL WRITE NODE ESCRIBE EL MENSAJE )
+
+function $inputValue(id: string) : string{
+    
+    const input = document.getElementById(id) as HTMLInputElement; // Lectura
+    let result = "";
+    
+    if(input){
+        result = input.value; // Aqui leo el valor
+    }
+    return result;
+}
+
+function $writeNode(id: string, msg: string) : void{
+  
+    const nodo = document.getElementById(id) as HTMLElement; //Escritura
+    if(nodo){
+        nodo.textContent = msg
     }
 }
